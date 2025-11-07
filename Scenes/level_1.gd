20932ccb1e5a53d8c39e316da13596bc962435bb
Bilal-Ahmad102 <ba2736180@gmail.com>
2025-11-08 00:49:57 +0500

    update in multiplayer

--- Scenes/level_1.gd
+++ Scenes/level_1.gd
@@ -8,14 +8,34 @@ extends Node3D
 @onready var waiting_room: Node3D = $waiting_room
 
 
+@rpc("authority", "call_local", "reliable")
 func shift_to_level():
-	if Multiplayer.is_host:
+	"""Transitions from waiting room to actual game level - called by host, executed on all clients"""
+	print("Shifting to level...")
+	
+	# Remove waiting room environment
+	if world_environment and is_instance_valid(world_environment):
 		world_environment.queue_free()
-		add_child(enviroment.instantiate())
+	
+	# Add game environment
+	if enviroment:
+		var env_instance = enviroment.instantiate()
+		add_child(env_instance)
+	
+	# Update spawner to use level spawn point
+	if multiplayer_spawner and level_spawn_point:
 		multiplayer_spawner.spawn_path = level_spawn_point.get_path()
-		multiplayer_spawner._spawn_path = level_spawn_point
+	
+	# Move all players from waiting room to level
+	if waiting_spawn_path and level_spawn_point:
+		for player in waiting_spawn_path.get_children():
+			if player is CharacterBody3D:
+				print("Moving player %s to level spawn" % player.name)
+				player.move_to_level()
+				player.reparent(level_spawn_point)
+	
+	# Remove waiting room
+	if waiting_room and is_instance_valid(waiting_room):
 		waiting_room.queue_free()
-
-		for player : CharacterBody3D in waiting_spawn_path.get_children():
-			player.move_to_level()
-			player.reparent(level_spawn_point)
+	
+	print("Level transition complete!")
--- Steam_Multiplayer/waiiting_room.gd
+++ Steam_Multiplayer/waiiting_room.gd
@@ -4,42 +4,68 @@ extends Node
 @onready var players_names: VBoxContainer = $Control/players_names
 @onready var spawn_path: Node3D = $spawn_path
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	start_game.pressed.connect(_on_start_game_pressed)
-	if !Multiplayer.is_host:
-		
-		print("Selected level : ",Multiplayer.selected_game_level) 
+	
+	# Only host can see/click start button
+	if Multiplayer and Multiplayer.is_host:
+		start_game.disabled = false
+		start_game.visible = true
+		print("I am host, can start game")
 	else:
-		print("I am host and selected level : ", Multiplayer.selected_game_level)
+		start_game.disabled = true
+		start_game.visible = false
+		print("I am client, waiting for host")
+	
+	if Multiplayer:
+		print("Selected level: ", Multiplayer.selected_game_level)
 
 func _on_start_game_pressed():
-	get_tree().get_first_node_in_group("level_controller").shift_to_level()
-	Multiplayer.start_game()
-func _process(delta: float) -> void:
-	add_players_ui()
-func add_players_ui():
-	# Get all players
-	var players = Multiplayer.get_current_players()
-	for player in players_names.get_children():
-		player.queue_free()
-		
-	for player in players:
-		var player_info_label : Label = Label.new()
-		players_names.add_child(player_info_label)
-		player_info_label.text = " "+str(player.steam_name)
+	# Host triggers the level shift
+	if Multiplayer and Multiplayer.is_host:
+		print("Host starting game...")
+		var level_controller = get_tree().get_first_node_in_group("level_controller")
+		if level_controller:
+			# Call the RPC which will execute on all clients
+			level_controller.shift_to_level.rpc()
+		else:
+			print("ERROR: Could not find level_controller!")
+	else:
+		print("ERROR: Only host can start the game!")
 
-	# Get player count for UI
-	#var count = Multiplayer.get_current_player_count()
-	#player_count_label.text = "%d/%d Players" % [count, Multiplayer.lobby_members_max]
-#
-	## Get full info
-	#var info = Multiplayer.get_players_info()
-	#if info.in_waiting_room:
-		#status_label.text = "Waiting for players... (%d/%d)" % [info.player_count, info.max_players]
-	#else:
-		#status_label.text = "Game in progress!"
-#
-	## Check if full
-	#if Multiplayer.is_lobby_full():
-		#start_button.disabled = false
+func _process(delta: float) -> void:
+	update_players_ui()
+	update_start_button()
+
+func update_players_ui():
+	"""Updates the list of players in the waiting room"""
+	if not Multiplayer:
+		return
+	
+	# Clear existing labels
+	for child in players_names.get_children():
+		child.queue_free()
+	
+	# Add label for each player
+	var players = Multiplayer.get_current_players()
+	for player in players:
+		var player_label = Label.new()
+		player_label.text = " " + str(player.steam_name)
+		players_names.add_child(player_label)
+
+func update_start_button():
+	"""Enable start button when lobby is full (optional auto-start logic)"""
+	if not Multiplayer or not Multiplayer.is_host:
+		return
+	
+	var player_count = Multiplayer.get_current_player_count()
+	var max_players = Multiplayer.lobby_members_max
+	
+	# Enable button when at least 2 players, or customize this logic
+	if player_count >= 2:
+		start_game.disabled = false
+		start_game.text = "Start Game (%d/%d)" % [player_count, max_players]
+	else:
+		start_game.disabled = true
+		start_game.text = "Waiting for Players... (%d/%d)" % [player_count, max_players]
