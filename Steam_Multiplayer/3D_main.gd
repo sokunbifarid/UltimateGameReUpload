bf9d3b17c4ff5a2879c456645752797651825605
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2025-12-23 07:53:59 +0100

    update

--- Steam_Multiplayer/3D_main.gd
+++ Steam_Multiplayer/3D_main.gd
@@ -27,51 +27,57 @@ func _ready() -> void:
 	
 	await get_tree().process_frame
 	
-	if not Steam.isSteamRunning():
-		print("ERROR: Steam is not running!")
-		#host.disabled = true
-		#refresh.disabled = true
-		return
-	
-	steam_id = Steam.getSteamID()
-	steam_username = Steam.getPersonaName()
-	
-	if steam_id == 0 or steam_username == "":
-		print("ERROR: Failed to get Steam user data!")
-		#host.disabled = true
-		#refresh.disabled = true
-		return
-	
-	print("Steam initialized - ID: %s, Name: %s" % [steam_id, steam_username])
-	
-	#host.pressed.connect(_on_host_connected)
-	#refresh.pressed.connect(_on_refresh_pressed)
-	
-	Steam.lobby_created.connect(_on_lobby_created)
-	Steam.lobby_joined.connect(_on_lobby_joined)
-	Steam.lobby_chat_update.connect(_on_lobby_chat_update)
-	Steam.persona_state_change.connect(_on_persona_change)
-	Steam.lobby_match_list.connect(_on_3D_lobby_match_list)
-	
-	# Connect multiplayer signals
-	multiplayer.peer_connected.connect(_on_peer_connected)
-	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
-	
-	ms.spawn_function = spawn_level
-	
-	open_lobby_list()
+
+
+#Developer commented code
+	#
+	#if not Steam.isSteamRunning():
+		#print("ERROR: Steam is not running!")
+		##host.disabled = true
+		##refresh.disabled = true
+		#return
+	#
+	#steam_id = Steam.getSteamID()
+	#steam_username = Steam.getPersonaName()
+	#
+	#if steam_id == 0 or steam_username == "":
+		#print("ERROR: Failed to get Steam user data!")
+		##host.disabled = true
+		##refresh.disabled = true
+		#return
+	#
+	#print("Steam initialized - ID: %s, Name: %s" % [steam_id, steam_username])
+	#
+	##host.pressed.connect(_on_host_connected)
+	##refresh.pressed.connect(_on_refresh_pressed)
+	#
+	#Steam.lobby_created.connect(_on_lobby_created)
+	#Steam.lobby_joined.connect(_on_lobby_joined)
+	#Steam.lobby_chat_update.connect(_on_lobby_chat_update)
+	#Steam.persona_state_change.connect(_on_persona_change)
+	#Steam.lobby_match_list.connect(_on_3D_lobby_match_list)
+	#
+	## Connect multiplayer signals
+	#multiplayer.peer_connected.connect(_on_peer_connected)
+	#multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+	#
+	#ms.spawn_function = spawn_level
+	#
+	#open_lobby_list()
 
 func _on_3D_lobby_match_list(lobbies):
 	lobbies_refreshed.emit(lobbies)
 
 func _process(_delta: float) -> void:
-	Steam.run_callbacks()
+	#developer commented code
+	#Steam.run_callbacks()
 	
 	# Update player count periodically, not every frame
 	var current_time = Time.get_ticks_msec() / 1000.0
 	if current_time - last_lobby_update_time >= LOBBY_UPDATE_INTERVAL:
 		_update_cached_player_count()
 		last_lobby_update_time = current_time
+
 func spawn_level(data):
 	var scene = load(data) as PackedScene
 	return scene.instantiate()
@@ -91,7 +97,9 @@ func _on_host_connected():
 	print("Creating lobby...")
 	notification.emit("Creating lobby...")
 	is_host = true
-	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, lobby_members_max)
+	
+	#developer commented code
+	#Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, lobby_members_max)
 
 # Leave current lobby
 func leave_current_lobby():
@@ -107,8 +115,9 @@ func leave_current_lobby():
 		multiplayer.multiplayer_peer = null
 		print("Disconnected from multiplayer peer")
 	
+	#developer commented code
 	# Leave Steam lobby
-	Steam.leaveLobby(lobby_id)
+	#Steam.leaveLobby(lobby_id)
 	
 	# Reset state
 	lobby_id = 0
@@ -126,11 +135,14 @@ func join_lobby(this_lobby_id: int):
 		leave_current_lobby()
 		await get_tree().create_timer(0.5).timeout  # Wait for clean disconnect
 	
-	print("Attempting to join lobby %s" % this_lobby_id)
-	notification.emit("join lobby %s" % Steam.getLobbyData(this_lobby_id,"name"))
-	lobby_members.clear()
-	is_host = false
-	Steam.joinLobby(this_lobby_id)
+
+#developer commented code
+	#print("Attempting to join lobby %s" % this_lobby_id)
+	#notification.emit("join lobby %s" % Steam.getLobbyData(this_lobby_id,"name"))
+	#lobby_members.clear()
+	#is_host = false
+	#Steam.joinLobby(this_lobby_id)
+
 func _on_lobby_created(connect: int, this_lobby_id: int) -> void:
 	print("Lobby created callback - connect: %s, lobby_id: %s" % [connect, this_lobby_id])
 	
@@ -140,28 +152,30 @@ func _on_lobby_created(connect: int, this_lobby_id: int) -> void:
 		
 		MultiplayerGlobal.selected_level = levels.pick_random()
 		
-		Steam.setLobbyJoinable(lobby_id, true)
-		Steam.setLobbyData(lobby_id, "name", str(steam_username + "'s Lobby"))
-		Steam.setLobbyData(lobby_id, "mode", "game")
-		Steam.setLobbyData(lobby_id, "level", MultiplayerGlobal.selected_level.resource_path)
 
-		# Create host peer
-		var peer = SteamMultiplayerPeer.new()
-		var error = peer.create_host(0)
-		if error != OK:
-			print("ERROR: Failed to create host: %s" % error)
-			return
-			
-		multiplayer.multiplayer_peer = peer
-		print("HOST: Multiplayer peer created with ID: %s" % multiplayer.get_unique_id())
-		
-		var set_relay: bool = Steam.allowP2PPacketRelay(true)
-		print("Allowing Steam to be relay backup: %s" % set_relay)
-		
-		get_lobby_members()
-		
-		get_tree().change_scene_to_packed(MultiplayerGlobal.selected_level)  # Uncomment this line
-		has_spawned_level = true
+#developer commented code
+		#Steam.setLobbyJoinable(lobby_id, true)
+		#Steam.setLobbyData(lobby_id, "name", str(steam_username + "'s Lobby"))
+		#Steam.setLobbyData(lobby_id, "mode", "game")
+		#Steam.setLobbyData(lobby_id, "level", MultiplayerGlobal.selected_level.resource_path)
+#
+		## Create host peer
+		#var peer = SteamMultiplayerPeer.new()
+		#var error = peer.create_host(0)
+		#if error != OK:
+			#print("ERROR: Failed to create host: %s" % error)
+			#return
+			#
+		#multiplayer.multiplayer_peer = peer
+		#print("HOST: Multiplayer peer created with ID: %s" % multiplayer.get_unique_id())
+		#
+		#var set_relay: bool = Steam.allowP2PPacketRelay(true)
+		#print("Allowing Steam to be relay backup: %s" % set_relay)
+		#
+		#get_lobby_members()
+		#
+		#get_tree().change_scene_to_packed(MultiplayerGlobal.selected_level)  # Uncomment this line
+		#has_spawned_level = true
 		
 		
 	else:
@@ -172,71 +186,73 @@ func _on_lobby_created(connect: int, this_lobby_id: int) -> void:
 func _on_lobby_joined(this_lobby_id: int, _permissions: int, _locked: bool, response: int) -> void:
 	print("Lobby joined callback - lobby_id: %s, response: %s" % [this_lobby_id, response])
 	
-	if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
-		lobby_id = this_lobby_id
-		print("Joined lobby: %s" % lobby_id)
-		
-		get_lobby_members()
+##developer commented code
+	#if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
+		#lobby_id = this_lobby_id
+		#print("Joined lobby: %s" % lobby_id)
+		#
+		#get_lobby_members()
 
 		# Only create client peer if we're not the host
-		if not is_host and lobby_members.size() > 0:
-			var peer = SteamMultiplayerPeer.new()
-			var host_steam_id = lobby_members[0]["steam_id"]
-			
-			print("CLIENT: Connecting to host Steam ID: %s" % host_steam_id)
-			var error = peer.create_client(host_steam_id, 0)
-			if error != OK:
-				print("ERROR: Failed to create client: %s" % error)
-				return
-				
-			multiplayer.multiplayer_peer = peer
-			print("CLIENT: Multiplayer peer created with ID: %s" % multiplayer.get_unique_id())
-			
-			# Wait a moment for connection to establish
-			await get_tree().create_timer(0.5).timeout
+		#if not is_host and lobby_members.size() > 0:
+			#var peer = SteamMultiplayerPeer.new()
+			#var host_steam_id = lobby_members[0]["steam_id"]
+			#
+			#print("CLIENT: Connecting to host Steam ID: %s" % host_steam_id)
+			#var error = peer.create_client(host_steam_id, 0)
+			#if error != OK:
+				#print("ERROR: Failed to create client: %s" % error)
+				#return
+				#
+			#multiplayer.multiplayer_peer = peer
+			#print("CLIENT: Multiplayer peer created with ID: %s" % multiplayer.get_unique_id())
+			#
+			## Wait a moment for connection to establish
+			#await get_tree().create_timer(0.5).timeout
+#
+			## Client spawns level AFTER connection is established
+			## Replace the client spawn section with:
+			#if not has_spawned_level:
+				#var level_path: String = Steam.getLobbyData(lobby_id, "level")
+				#
+				#if level_path != "":
+					#print("CLIENT: Loading level from lobby data: %s" % level_path)
+					#var level_scene = load(level_path) as PackedScene
+					#get_tree().change_scene_to_packed(level_scene)
+					#has_spawned_level = true
+				#else:
+					#print("CLIENT: No level data found in lobby")
+		
+	#else:
+		#var fail_reason: String
+		#
+		#match response:
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_DOESNT_EXIST: 
+				#fail_reason = "This lobby no longer exists."
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_NOT_ALLOWED: 
+				#fail_reason = "You don't have permission to join this lobby."
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_FULL: 
+				#fail_reason = "The lobby is now full."
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_ERROR: 
+				#fail_reason = "Uh... something unexpected happened!"
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_BANNED: 
+				#fail_reason = "You are banned from this lobby."
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_LIMITED: 
+				#fail_reason = "You cannot join due to having a limited account."
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_CLAN_DISABLED: 
+				#fail_reason = "This lobby is locked or disabled."
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_COMMUNITY_BAN: 
+				#fail_reason = "This lobby is community locked."
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_MEMBER_BLOCKED_YOU: 
+				#fail_reason = "A user in the lobby has blocked you from joining."
+			#Steam.CHAT_ROOM_ENTER_RESPONSE_YOU_BLOCKED_MEMBER: 
+				#fail_reason = "A user you have blocked is in the lobby."
+			#_: 
+				#fail_reason = "Unknown error."
+		#
+		#print("Failed to join this chat room: %s" % fail_reason)
+		#open_lobby_list()
 
-			# Client spawns level AFTER connection is established
-			# Replace the client spawn section with:
-			if not has_spawned_level:
-				var level_path: String = Steam.getLobbyData(lobby_id, "level")
-				
-				if level_path != "":
-					print("CLIENT: Loading level from lobby data: %s" % level_path)
-					var level_scene = load(level_path) as PackedScene
-					get_tree().change_scene_to_packed(level_scene)
-					has_spawned_level = true
-				else:
-					print("CLIENT: No level data found in lobby")
-		
-	else:
-		var fail_reason: String
-		
-		match response:
-			Steam.CHAT_ROOM_ENTER_RESPONSE_DOESNT_EXIST: 
-				fail_reason = "This lobby no longer exists."
-			Steam.CHAT_ROOM_ENTER_RESPONSE_NOT_ALLOWED: 
-				fail_reason = "You don't have permission to join this lobby."
-			Steam.CHAT_ROOM_ENTER_RESPONSE_FULL: 
-				fail_reason = "The lobby is now full."
-			Steam.CHAT_ROOM_ENTER_RESPONSE_ERROR: 
-				fail_reason = "Uh... something unexpected happened!"
-			Steam.CHAT_ROOM_ENTER_RESPONSE_BANNED: 
-				fail_reason = "You are banned from this lobby."
-			Steam.CHAT_ROOM_ENTER_RESPONSE_LIMITED: 
-				fail_reason = "You cannot join due to having a limited account."
-			Steam.CHAT_ROOM_ENTER_RESPONSE_CLAN_DISABLED: 
-				fail_reason = "This lobby is locked or disabled."
-			Steam.CHAT_ROOM_ENTER_RESPONSE_COMMUNITY_BAN: 
-				fail_reason = "This lobby is community locked."
-			Steam.CHAT_ROOM_ENTER_RESPONSE_MEMBER_BLOCKED_YOU: 
-				fail_reason = "A user in the lobby has blocked you from joining."
-			Steam.CHAT_ROOM_ENTER_RESPONSE_YOU_BLOCKED_MEMBER: 
-				fail_reason = "A user you have blocked is in the lobby."
-			_: 
-				fail_reason = "Unknown error."
-		
-		print("Failed to join this chat room: %s" % fail_reason)
-		open_lobby_list()
 func _on_peer_connected(id: int):
 	print("Peer connected with multiplayer ID: %s" % id)
 
@@ -245,22 +261,24 @@ func _on_peer_disconnected(id: int):
 
 
 func _on_lobby_chat_update(this_lobby_id: int, change_id: int, making_change_id: int, chat_state: int) -> void:
-	var changer_name: String = Steam.getFriendPersonaName(change_id)
-	
-	if chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_ENTERED:
-		print("%s has joined the lobby." % changer_name)
-	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_LEFT:
-		print("%s has left the lobby." % changer_name)
-	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_KICKED:
-		print("%s has been kicked from the lobby." % changer_name)
-	elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_BANNED:
-		print("%s has been banned from the lobby." % changer_name)
-	else:
-		print("%s did... something." % changer_name)
-	
-	get_lobby_members()
-	# Immediately update cached count when lobby changes
-	_update_cached_player_count()
+	# developer commented code
+	#var changer_name: String = Steam.getFriendPersonaName(change_id)
+	#
+	#if chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_ENTERED:
+		#print("%s has joined the lobby." % changer_name)
+	#elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_LEFT:
+		#print("%s has left the lobby." % changer_name)
+	#elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_KICKED:
+		#print("%s has been kicked from the lobby." % changer_name)
+	#elif chat_state == Steam.CHAT_MEMBER_STATE_CHANGE_BANNED:
+		#print("%s has been banned from the lobby." % changer_name)
+	#else:
+		#print("%s did... something." % changer_name)
+	#
+	#get_lobby_members()
+	## Immediately update cached count when lobby changes
+	#_update_cached_player_count()
+	pass
 
 func _on_persona_change(this_steam_id: int, _flag: int) -> void:
 	if lobby_id > 0:
@@ -268,27 +286,29 @@ func _on_persona_change(this_steam_id: int, _flag: int) -> void:
 		get_lobby_members()
 
 func open_lobby_list():
-	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
-	print("Requesting a lobby list")
-	notification.emit("Requesting a lobby list")
-	Steam.requestLobbyList()
-
+	#developer commented code
+	#Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
+	#print("Requesting a lobby list")
+	#notification.emit("Requesting a lobby list")
+	#Steam.requestLobbyList()
+	pass
 
 func get_lobby_members() -> void:
-	lobby_members.clear()
-	var num_of_members: int = Steam.getNumLobbyMembers(lobby_id)
-	
-	#print("Getting %s lobby members..." % num_of_members)
-	
-	for this_member in range(0, num_of_members):
-		var member_steam_id: int = Steam.getLobbyMemberByIndex(lobby_id, this_member)
-		var member_steam_name: String = Steam.getFriendPersonaName(member_steam_id)
-		
-		lobby_members.append({"steam_id": member_steam_id, "steam_name": member_steam_name})
-	
-	# Update cached count when we get lobby members
-	cached_player_count = num_of_members
-
+	# developer commented code
+	#lobby_members.clear()
+	#var num_of_members: int = Steam.getNumLobbyMembers(lobby_id)
+	#
+	##print("Getting %s lobby members..." % num_of_members)
+	#
+	#for this_member in range(0, num_of_members):
+		#var member_steam_id: int = Steam.getLobbyMemberByIndex(lobby_id, this_member)
+		#var member_steam_name: String = Steam.getFriendPersonaName(member_steam_id)
+		#
+		#lobby_members.append({"steam_id": member_steam_id, "steam_name": member_steam_name})
+	#
+	## Update cached count when we get lobby members
+	#cached_player_count = num_of_members
+	pass
 
 
 func get_current_player_count():
@@ -297,20 +317,22 @@ func get_current_player_count():
 
 func _update_cached_player_count():
 	"""Update the cached player count periodically"""
-	if lobby_id > 0:
-		cached_player_count = Steam.getNumLobbyMembers(lobby_id)
+	#developer commented code
+	#if lobby_id > 0:
+		#cached_player_count = Steam.getNumLobbyMembers(lobby_id)
 
 func get_current_players():
-	lobby_members.clear()
-	var num_of_members: int = Steam.getNumLobbyMembers(lobby_id)
-	
-	#print("Getting %s lobby members..." % num_of_members)
-	
-	for this_member in range(0, num_of_members):
-		var member_steam_id: int = Steam.getLobbyMemberByIndex(lobby_id, this_member)
-		var member_steam_name: String = Steam.getFriendPersonaName(member_steam_id)
-		
-		lobby_members.append({"steam_id": member_steam_id, "steam_name": member_steam_name})
+	#developer commented code
+	#lobby_members.clear()
+	#var num_of_members: int = Steam.getNumLobbyMembers(lobby_id)
+	#
+	##print("Getting %s lobby members..." % num_of_members)
+	#
+	#for this_member in range(0, num_of_members):
+		#var member_steam_id: int = Steam.getLobbyMemberByIndex(lobby_id, this_member)
+		#var member_steam_name: String = Steam.getFriendPersonaName(member_steam_id)
+		#
+		#lobby_members.append({"steam_id": member_steam_id, "steam_name": member_steam_name})
 	
 	return lobby_members
 
--- Steam_Multiplayer/online_menu.gd
+++ Steam_Multiplayer/online_menu.gd
@@ -30,72 +30,75 @@ func _on_3D_lobby_match_list(these_lobbies: Array) -> void:
 	# Hide the template lobby
 	lobby.hide()
 	
-	# Clear existing lobby instances (keep the template)
-	for lobbi in lobbies_container.get_children():
-		if lobbi.name == "lobby": 
-			continue
-		lobbi.queue_free()
-	
-	# Reset position counter
-	curr_pos.y = 0
-	
-	# Create lobby entries for each received lobby
-	for this_lobby in these_lobbies:
-		var lobby_name: String = Steam.getLobbyData(this_lobby, "name")
-		var lobby_mode: String = Steam.getLobbyData(this_lobby, "mode")
-		var lobby_num_members: int = Steam.getNumLobbyMembers(this_lobby)
-		var lobby_max_members: int = Steam.getLobbyMemberLimit(this_lobby)
-		
-		# Skip unnamed lobbies
-		if lobby_name.length() == 0:
-			continue
-		
-		# Format the display text with player count
-		var display_text = "%s (%d/%d)" % [lobby_name, lobby_num_members, lobby_max_members]
-		
-		# Duplicate the template lobby
-		var new_lobby: Node3D = lobby.duplicate()
-		new_lobby.name = "lobby_%s" % this_lobby
-		
-		# Set the lobby display text with player count
-		var label = new_lobby.get_node("Label3D")
-		label.text = display_text
-		
-		# Optional: Change color based on lobby fullness
-		if lobby_num_members >= lobby_max_members:
-			label.modulate = Color(0.8, 0.3, 0.3)  # Red tint for full lobbies
-		elif lobby_num_members >= lobby_max_members * 0.75:
-			label.modulate = Color(1.0, 0.8, 0.3)  # Yellow tint for nearly full
-		else:
-			label.modulate = Color(0.3, 1.0, 0.5)  # Green tint for available
-		
-		# Store lobby metadata
-		new_lobby.set_meta("lobby_id", this_lobby)
-		new_lobby.set_meta("lobby_name", lobby_name)
-		new_lobby.set_meta("lobby_mode", lobby_mode)
-		new_lobby.set_meta("num_members", lobby_num_members)
-		new_lobby.set_meta("max_members", lobby_max_members)
-		
-		# Add to container
-		lobbies_container.add_child(new_lobby)
-		
-		# Connect input event
-		var area: Area3D = new_lobby.get_node("Area3D")
-		area.input_event.connect(_on_area_3d_input_event.bind(this_lobby, lobby_num_members, lobby_max_members))
-		
-		# Add hover effects
-		area.mouse_entered.connect(_on_lobby_hover.bind(new_lobby))
-		area.mouse_exited.connect(_on_lobby_unhover.bind(new_lobby))
-		
-		# Position the lobby entry
-		new_lobby.position = Vector3(0, curr_pos.y, 0)
-		new_lobby.show()
-		
-		# Increment position for next lobby
-		curr_pos.y += 1.5
-	
-	print("Total lobbies displayed: ", lobbies_container.get_child_count() - 1)
-	#emit_signal("Multiplayer.notification","Total lobbies displayed: "+ str(lobbies_container.get_child_count() - 1))
+# Developer commented code
+	## Clear existing lobby instances (keep the template)
+	#for lobbi in lobbies_container.get_children():
+		#if lobbi.name == "lobby": 
+			#continue
+		#lobbi.queue_free()
+	#
+	## Reset position counter
+	#curr_pos.y = 0
+	#
+	## Create lobby entries for each received lobby
+	#for this_lobby in these_lobbies:
+		#var lobby_name: String = Steam.getLobbyData(this_lobby, "name")
+		#var lobby_mode: String = Steam.getLobbyData(this_lobby, "mode")
+		#var lobby_num_members: int = Steam.getNumLobbyMembers(this_lobby)
+		#var lobby_max_members: int = Steam.getLobbyMemberLimit(this_lobby)
+		#
+		## Skip unnamed lobbies
+		#if lobby_name.length() == 0:
+			#continue
+		#
+		## Format the display text with player count
+		#var display_text = "%s (%d/%d)" % [lobby_name, lobby_num_members, lobby_max_members]
+		#
+		## Duplicate the template lobby
+		#var new_lobby: Node3D = lobby.duplicate()
+		#new_lobby.name = "lobby_%s" % this_lobby
+		#
+		## Set the lobby display text with player count
+		#var label = new_lobby.get_node("Label3D")
+		#label.text = display_text
+		#
+		## Optional: Change color based on lobby fullness
+		#if lobby_num_members >= lobby_max_members:
+			#label.modulate = Color(0.8, 0.3, 0.3)  # Red tint for full lobbies
+		#elif lobby_num_members >= lobby_max_members * 0.75:
+			#label.modulate = Color(1.0, 0.8, 0.3)  # Yellow tint for nearly full
+		#else:
+			#label.modulate = Color(0.3, 1.0, 0.5)  # Green tint for available
+		#
+		## Store lobby metadata
+		#new_lobby.set_meta("lobby_id", this_lobby)
+		#new_lobby.set_meta("lobby_name", lobby_name)
+		#new_lobby.set_meta("lobby_mode", lobby_mode)
+		#new_lobby.set_meta("num_members", lobby_num_members)
+		#new_lobby.set_meta("max_members", lobby_max_members)
+		#
+		## Add to container
+		#lobbies_container.add_child(new_lobby)
+		#
+		## Connect input event
+		#var area: Area3D = new_lobby.get_node("Area3D")
+		#area.input_event.connect(_on_area_3d_input_event.bind(this_lobby, lobby_num_members, lobby_max_members))
+		#
+		## Add hover effects
+		#area.mouse_entered.connect(_on_lobby_hover.bind(new_lobby))
+		#area.mouse_exited.connect(_on_lobby_unhover.bind(new_lobby))
+		#
+		## Position the lobby entry
+		#new_lobby.position = Vector3(0, curr_pos.y, 0)
+		#new_lobby.show()
+		#
+		## Increment position for next lobby
+		#curr_pos.y += 1.5
+	#
+	#print("Total lobbies displayed: ", lobbies_container.get_child_count() - 1)
+	#leave the below commented
+	##emit_signal("Multiplayer.notification","Total lobbies displayed: "+ str(lobbies_container.get_child_count() - 1))
+
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int, this_lobby: int, num_members: int, max_members: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		# Check if lobby is full before attempting to join
