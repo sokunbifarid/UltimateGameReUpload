3b2ae511b59402c4fe73a25bd5cb38b14f4c2070
sokunbi farid <sokunbifarid1@gmail.com>
2025-12-24 15:16:23 +0100

    update

--- Scripts/player_match_stats.gd
+++ Scripts/player_match_stats.gd
@@ -6,8 +6,16 @@ extends VBoxContainer
 var target_player: CharacterBody3D
 
 func set_monitor(target: CharacterBody3D):
-	target_player = target
-	target_player.UpdatePlayerStats.connect(set_player_stats)
+	await get_tree().process_frame
+	await get_tree().process_frame
+	if target:
+		target_player = target
+		health_progress_bar_node.value = target.health
+		powerup_progress_bar_node.value = target.powerup_exp
+		target_player.UpdatePlayerStats.connect(set_player_stats)
+		print("target assigned: " + str(target))
+	else:
+		print("target is null")
 
 func set_player_stats(health: int, powerup_exp: int):
 	health_progress_bar_node.value = health
